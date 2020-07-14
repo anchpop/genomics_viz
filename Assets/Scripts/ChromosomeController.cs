7cd2d9b808b3d6103249459703255dc156f89202
Andre Popovitch <andre@popovit.ch>
2020-07-14 02:09:23 -0400

    added snazzier simplification code

--- Assets/Scripts/ChromosomeController.cs
+++ Assets/Scripts/ChromosomeController.cs
@@ -2,29 +2,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using UnityEngine;
+
+public struct Point
+{
+    public Vector3 position;
+    public int basePairIndex;
+}
 
 public class ChromosomeController : MonoBehaviour
 {
     public float overallScale = 1.5f;
-    public float linewidth = 15;
-    public int simplificationFactor = 1;
-    private int numberOfRows = 0;
+    public float linewidth = 1;
+    public float simplificationFactor = 0;
+    public bool smartSimplify = true;
     public TextAsset locationSequence;
-    public List<Vector3> locations;
+    private List<Point> points;
 
     public GameObject spherePrefab;
     public GameObject cylinderPrefab;
 
-    LineRenderer line;
-    // Start is called before the first frame update
+    private LineRenderer line;
+    private int numberOfRows = 0;
+    private List<int> removalOrder;
+
     void Start()
     {
         var center = Vector3.zero;
         line = GetComponent<LineRenderer>();
 
         var pointsRaw = new List<Vector3>();
-        var points = new List<Vector3>();
+        points = new List<Point>();
 
         Vector3 min = Vector3.zero;
         Vector3 max = Vector3.zero;
@@ -35,8 +44,7 @@ public class ChromosomeController : MonoBehaviour
             if (line != "")
             {
                 numberOfRows++;
-                if (numberOfRows % simplificationFactor != 0) continue;
-
+                if (!smartSimplify && UnityEngine.Random.value < simplificationFactor) continue;
 
                 var coords = line.Split('\t');
                 var newVector = new Vector3(float.Parse(coords[0]), float.Parse(coords[1]), float.Parse(coords[2]));
@@ -75,12 +83,27 @@ public class ChromosomeController : MonoBehaviour
 
         center = center / pointsRaw.Count;
 
+        var count = 0;
         foreach (var point in pointsRaw)
         {
+            count++;
             var scaling = Mathf.Max(Mathf.Max(min.x - max.x, min.y - max.y), min.z - max.z);
+            var p = new Point();
+            p.position = (point - center) * overallScale / scaling;
+            p.basePairIndex = count * 5000;
 
-            points.Add((point - center) * overallScale / scaling);
+            points.Add(p);
         }
+
+        if (smartSimplify)
+        {
+            removalOrder = GetSimplificationOrder(points);
+            for (var i = 0; i < numberOfRows * simplificationFactor; i++)
+            {
+                points.RemoveAt(removalOrder[i]);
+            }
+        }
+
 
         for (int i = 0; i < points.Count - 1; i++)
         {
@@ -95,10 +118,70 @@ public class ChromosomeController : MonoBehaviour
         line.alignment = LineAlignment.View;*/
     }
 
-    void AddLineSegment(Vector3 p1, Vector3 p2)
+    List<int> GetSimplificationOrder(List<Point> points)
     {
-        var obj = Instantiate(cylinderPrefab, ((p1 + p2) / 2), Quaternion.LookRotation(p1 - p2, Vector3.right), transform);
-        obj.transform.localScale = new Vector3(linewidth / 100, linewidth / 100, (p1 - p2).magnitude);
+        var importanceList = new List<(Point point, float area)>();
+        var removalOrder = new List<int>();
+
+        // generate initial importance list
+        for (int i = 1; i < points.Count - 1; i++)
+        {
+            var a = points[i].position;
+            var b = points[i + 1].position;
+            var c = points[i - 1].position;
+            var area = triangleArea(a, b, c);
+            importanceList.Add((points[i], area));
+        }
+
+
+        while (importanceList.Count > 1)
+        {
+            // find index of least important point
+            int lowestIndex = 0;
+            for (int i = 1; i < importanceList.Count; i++)
+            {
+                if (importanceList[lowestIndex].area > importanceList[i].area)
+                {
+                    lowestIndex = i;
+                }
+            }
+
+            // This is the point to remove!
+            removalOrder.Add(lowestIndex);
+            importanceList.RemoveAt(lowestIndex);
+
+            // Recalcuate the importance of the neighbour points
+            foreach (var index in new List<int> { lowestIndex - 1, lowestIndex })
+            {
+                // if we're at the very start or the very end of the *point* list, we don't simplify anything
+                if (index == -1) continue;
+                if (index == importanceList.Count) continue;
+
+                // if we're at the very start of the *importance* list, get the first point from the *point* list as the "before" point
+                var before = index == 0 ? points[0].position : importanceList[index - 1].point.position;
+                var current = importanceList[index].point.position;
+                // if we're at the very end of the *importance* list, get the last point from the *point* list as the "after" point
+                var next = index == importanceList.Count - 1 ? points[points.Count - 1].position : importanceList[index + 1].point.position;
+
+                var area = triangleArea(before, current, next);
+                importanceList[index] = (importanceList[index].point, area);
+            }
+
+            // Rinse and repeat until there's no more points left to remove! This will give us an order of points to remove when simplifying
+        }
+
+        return removalOrder;
+    }
+
+    void AddLineSegment(Point p1, Point p2)
+    {
+        var obj = Instantiate(cylinderPrefab, ((p1.position + p2.position) / 2), Quaternion.LookRotation(p1.position - p2.position, Vector3.right), transform);
+        obj.transform.localScale = new Vector3(linewidth / 100, linewidth / 100, (p1.position - p2.position).magnitude);
+    }
+
+    float triangleArea(Vector3 a, Vector3 b, Vector3 c)
+    {
+        return Mathf.Abs(a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y));
     }
 
     // Update is called once per frame
