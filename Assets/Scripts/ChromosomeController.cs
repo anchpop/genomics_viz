417b330c89e61113b6b5905d3f02198d15dc638a
Andre Popovitch <andre@popovit.ch>
2020-07-13 22:34:26 -0400

    got orientation working properly

--- Assets/Scripts/ChromosomeController.cs
+++ Assets/Scripts/ChromosomeController.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 
 public class ChromosomeController : MonoBehaviour
 {
+    public float overallScale = 1.5f;
     public float linewidth = 15;
+    public int simplificationFactor = 1;
+    private int numberOfRows = 0;
     public TextAsset locationSequence;
     public List<Vector3> locations;
 
@@ -26,10 +29,15 @@ public class ChromosomeController : MonoBehaviour
         Vector3 min = Vector3.zero;
         Vector3 max = Vector3.zero;
 
+
         foreach (var line in locationSequence.text.Split('\n'))
         {
             if (line != "")
             {
+                numberOfRows++;
+                if (numberOfRows % simplificationFactor != 0) continue;
+
+
                 var coords = line.Split('\t');
                 var newVector = new Vector3(float.Parse(coords[0]), float.Parse(coords[1]), float.Parse(coords[2]));
 
@@ -71,7 +79,7 @@ public class ChromosomeController : MonoBehaviour
         {
             var scaling = Mathf.Max(Mathf.Max(min.x - max.x, min.y - max.y), min.z - max.z);
 
-            points.Add((point - center) / scaling);
+            points.Add((point - center) * overallScale / scaling);
         }
 
         for (int i = 0; i < points.Count - 1; i++)
@@ -90,7 +98,7 @@ public class ChromosomeController : MonoBehaviour
     void AddLineSegment(Vector3 p1, Vector3 p2)
     {
         var obj = Instantiate(cylinderPrefab, ((p1 + p2) / 2), Quaternion.LookRotation(p1 - p2, Vector3.right), transform);
-        obj.transform.localScale = new Vector3(obj.transform.localScale.x, (p1 - p2).magnitude, obj.transform.localScale.z);
+        obj.transform.localScale = new Vector3(linewidth / 100, linewidth / 100, (p1 - p2).magnitude);
     }
 
     // Update is called once per frame
