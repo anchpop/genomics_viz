eba358e8a657fb1958468c19bf9df8eeac3943ab
Andre Popovitch <andre@popovit.ch>
2020-07-27 23:56:35 -0400

    faster zoom

--- Assets/Scripts/CameraParentController.cs
+++ Assets/Scripts/CameraParentController.cs
@@ -8,7 +8,7 @@ public class CameraParentController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -26,11 +26,11 @@ public class CameraParentController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.localScale = transform.localScale * (1 - (.1f * Time.deltaTime));
+            transform.localScale = transform.localScale * (1 - (.4f * Time.deltaTime));
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.localScale = transform.localScale * (1 + (.1f * Time.deltaTime));
+            transform.localScale = transform.localScale * (1 + (.4f * Time.deltaTime));
         }
     }
 }
